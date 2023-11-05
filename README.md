d9f24cbda612e22c3935365011850e2357a75451
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-11-05 22:51:00 +0530

    project connected with MongoDB Driver successful

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-server side link : 
+server side github link : https://github.com/Porgramming-Hero-web-course/b8a11-server-side-mr-tnmy-srkr
+
+server side vercel link : https://online-group-study-mern-assignment-11-server.vercel.app/
--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+var jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//mongodb connection string
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@groupstudycluster.bgrpf96.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+    // Send a ping to confirm a successful connection
+    client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
