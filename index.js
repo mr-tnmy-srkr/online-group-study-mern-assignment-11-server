f362620fa584aefd9c3a473230b2e7f4696f0327
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-11-06 22:29:36 +0530

    assignments post and get api end point created

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
 //mongodb connection string
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@groupstudycluster.bgrpf96.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -16,16 +26,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+const assignmentCollection = client
+  .db("assignmentDB")
+  .collection("assignments");
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     client.connect();
+
+    app.get("/api/v1/assignments", async (req, res) => {
+      const cursor = assignmentCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/api/v1/user/create-assignment", async (req, res) => {
+      const data = req.body;
+      // console.log(data);
+      const result = await assignmentCollection.insertOne(data);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -33,11 +63,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
